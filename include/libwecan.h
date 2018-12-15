bd87d5b75b439f54d984c4042acbd506b8c8e2c1
nisennenmondai <nisennenmondai@protonmail.com>
2018-12-15 11:15:23 +0100

    [DOC] add comments for better understanding

--- include/libwecan.h
+++ include/libwecan.h
@@ -12,6 +12,15 @@
 #define INTEL     4
 #define MOTOROLA  5
 
+/**
+ * @brief return byte indexes of msb, lsb and lsb bit offset from signal parameters
+ * @param startbit [In] signal startbit = lsb in case of little-endian, msb else
+ * @param length   [In] signal length in bits
+ * @param endianness  true if signal data is in little endian format
+ * @param offset_lsb     [Out] offset (0..7) of the lsb in its containing byte
+ * @param byte_index_lsb [Out] byte index of the lsb from the matrix start
+ * @param byte_index_msb [Out] byte index of the msb from the matrix start
+ */
 void compute_indexes(const uint16_t startbit, const uint8_t length, uint8_t endianness, 
                 uint8_t *offset_lsb, uint8_t *byte_index_lsb, uint8_t *byte_index_msb)
 {
@@ -19,6 +28,12 @@ void compute_indexes(const uint16_t startbit, const uint8_t length, uint8_t endi
         uint16_t lsb = 0;
 
         if (endianness == MOTOROLA) {
+                /* 
+                 * big-endian, compute lsb from msb using dbc bit numbering 
+                 * definition from signal msb count down length-1 time, if bit 
+                 * reachs bit_0 of current byte jump to bit_7 of next byte 
+                 * (left to right bit numbering from msb to lsb)
+                 */
                 msb =  startbit;
                 uint16_t current_bit_nr = startbit;
 
@@ -37,8 +52,8 @@ void compute_indexes(const uint16_t startbit, const uint8_t length, uint8_t endi
                 *byte_index_msb = msb / 8;
                 *offset_lsb = lsb % 8;
 
-        } else { /* is little endian */
-                /* calcul index */
+        } else { 
+                /* little-endian, right to left bit numbering from lsb to msb */
                 lsb = startbit;
                 msb = lsb + length - 1;
                 *byte_index_msb = msb / 8;
@@ -47,17 +62,22 @@ void compute_indexes(const uint16_t startbit, const uint8_t length, uint8_t endi
         }
 }
 
+/**
+ * @brief copy 8 bytes data chunk containing signal to a local temp var
+ *        the endianness is taken in account
+ * @param frame  frame data address
+ * @param byte_index_lsb  byte index of the lsb from the frame[0]
+ * @param byte_index_msb  byte index of the msb from the frame[0]
+ * @param endianness
+ * @return uint64 in little endian format containing the signal to extract
+ */
 uint64_t frame_to_local(const uint8_t *frame, uint8_t byte_index_lsb, 
                 uint8_t byte_index_msb, uint8_t endianness)
 {
-        uint64_t target = 0;
-
-        /* 
-         * do a cross copy of bytes containing signal (to little endian)
-         * 0F EA FF --> FF EA 0F 
-         */
+        uint64_t target = frame[byte_index_lsb];
         uint8_t *dest = (uint8_t*) &target;
-        target = frame[byte_index_lsb];
+
+        /* big-endian => do a reverse copy of bytes (aka copy and swap) */
         if (endianness == MOTOROLA) {
 
                 int i;
@@ -66,8 +86,8 @@ uint64_t frame_to_local(const uint8_t *frame, uint8_t byte_index_lsb,
                         *dest = frame[i];
                 }
 
-        } else { /* is little endian */
-                /* copy signal to target */
+        } else {
+                /* simple bytes copy little-endian -> little-endian */
                 int i;
                 for (i = byte_index_lsb + 1; i <= byte_index_msb; i++) {
                         dest++;
@@ -77,13 +97,21 @@ uint64_t frame_to_local(const uint8_t *frame, uint8_t byte_index_lsb,
         return target;
 }
 
+/**
+ * @brief extract a signal value from a CAN frame
+ * @param frame     frame data address
+ * @param startbit  signal start bit
+ * @param length    signal length in bits
+ * @param signedness
+ * @param endianness
+ * @return signal value as uint64 ready for decoding by using Factor and Offset
+ *         If the signal value is signed (and negative), just cast the extracted value to int64
+ *         to get its value thanks to the algo of sign bit extension
+ */
+
 uint64_t extract(const uint8_t *frame, const uint16_t startbit, 
                 const uint8_t length, uint8_t signedness, uint8_t endianness)
 {
-        /* 
-         * function returns the signal value in the can frame 
-         * if signal is negative, cast to int64_t 
-         */
         uint8_t byte_index_msb = 0;
         uint8_t byte_index_lsb = 0;
         uint8_t offset_lsb = 0;
@@ -100,16 +128,20 @@ uint64_t extract(const uint8_t *frame, const uint16_t startbit,
         //printf("startbit is: %d\n", startbit);
         //printf("byte_index_lsb: %d\nbyte_index_msb: %d\n", byte_index_lsb, byte_index_msb);
 
-        /* shift the signal to b0 of target */
+        /* remove lower bits not in signal (lower than offset lsb) */
         target = target >> offset_lsb;
 
-        /* clear bits after msb  ex:1111 1111 --> 0000 1111 */
+        /* clear higher bits not in signal (higher than msb) */
         uint64_t mask = ((uint64_t)1 << length) - 1;
         target = target & mask;
 
         /* sign extension */
         if (signedness == SIGNED) {
-                /*  we test if msb is at 1 (negative number), sign extension */
+                /* 
+                 * we test if msb has value 1 (negative number), do sign bit 
+                 * extension all bits higher than msb set to 1, so that a cast 
+                 * of target to int64 will provide a correct negative value
+                 */
                 if (((uint64_t)1 << (length - 1)) & target) {
                         uint64_t sign_ext_mask = ~mask;
                         target = target | sign_ext_mask;
@@ -118,6 +150,14 @@ uint64_t extract(const uint8_t *frame, const uint16_t startbit,
         return target;
 }
 
+/**
+ * @brief insert a signal value into a CAN frame
+ * @param frame     [In/Out] frame data address
+ * @param startbit  signal start bit
+ * @param length    signal length in bits
+ * @param value     signal value
+ * @param endianness
+ */
 void insert(uint8_t *frame, uint16_t startbit, uint8_t length, uint64_t value, 
                 uint8_t endianness)
 {
@@ -131,9 +171,11 @@ void insert(uint8_t *frame, uint16_t startbit, uint8_t length, uint64_t value,
 
         target = frame_to_local(frame, byte_index_lsb, byte_index_msb, endianness);
 
-        /* mask */
+        /* create mask to erase current signal value */
         uint64_t mask = ((uint64_t)1 << length) - 1;
         uint64_t erase_mask = ~(mask << offset_lsb);
+
+        /* erase and insert signal value into local data chunk */
         target = (target & erase_mask) | (value << offset_lsb);
 
         /* debug */
@@ -142,18 +184,21 @@ void insert(uint8_t *frame, uint16_t startbit, uint8_t length, uint64_t value,
         //printf("startbit is: %d\n", startbit);
         //printf("byte_index_lsb: %d\nbyte_index_msb: %d\n", byte_index_lsb, byte_index_msb);
 
-        /* copy local to frame */
+        /* 
+         * copy back data chunk containing signal into CAN frame by taken into
+         * account endianess
+         */
         uint8_t *src = (uint8_t*) &target;
         if (endianness == MOTOROLA) {
-
+                /* big-endian, reverse order copy (copy and swap) */
                 int i;
                 for (i = byte_index_lsb; i >= byte_index_msb; i--) {
                         frame[i] = *src;
                         src++;
                 }
 
-        } else { /* is little endian */
-
+        } else {
+                /* little-endian, simple bytes copy */
                 int i;
                 for (i = byte_index_lsb; i <= byte_index_msb; i++) {
                         frame[i] = *src;
@@ -162,6 +207,9 @@ void insert(uint8_t *frame, uint16_t startbit, uint8_t length, uint64_t value,
         }
 }
 
+/**
+ * @brief encode a physical value into CAN format and insert it into a frame
+ */
 void encode_uint64_t(uint8_t *frame, uint64_t physical_value, uint16_t startbit,
                 uint8_t length, uint8_t endianness, double factor, double offset)
 {
@@ -190,6 +238,10 @@ void encode_float(uint8_t *frame, float physical_value, uint16_t startbit,
         insert(frame, startbit, length, can_value, endianness);
 }
 
+/**
+ * @brief extract a signal and decode it into physical value
+ *
+ */
 uint64_t decode_uint64_t(uint8_t *frame, uint16_t startbit, uint8_t length, 
                 uint8_t endianness, double factor, double offset)
 {
